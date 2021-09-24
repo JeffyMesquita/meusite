f3731077500805f3eadb60a249eb48cd03d315dc
Jeferson Mesquita <je_2742@hotmail.com>
2021-09-24 00:36:01 -0300

    Manipulando Formulários pt1
    Manipulando Formulários pt1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      
+      nome: "",
+      email: "",
+      senha: "",
+      error: "",
     };
 
+    this.cadastrar = this.cadastrar.bind(this);
   }
 
-  render(){
-    return(
+  cadastrar(event) {
+    const {nome, email, senha} = this.state;
+    
+    if(nome !== '' && email !== '' && senha !== '') {
+      alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`);
+    } else {
+      this.setState({
+        error: 'Ops! Parece que está faltando algo!?'
+      })
+    }
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
       <div>
-        
+        <h1>Novo usuário</h1>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.cadastrar}>
+          <label>Nome:</label>
+          <input
+            type="text"
+            value={this.state.nome}
+            onChange={(e) => this.setState({ nome: e.target.value })}
+          />
+
+          <br />
+          <label>Email: </label>
+          <input
+            type="email"
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
+
+          <br />
+          <label>Senha:</label>
+          <input
+            type="password"
+            value={this.state.senha}
+            onChange={(e) => this.setState({ senha: e.target.value })}
+          />
+
+          <br />
+          <button type="submit">Cadastrar</button>
+        </form>
       </div>
-    ) 
+    );
   }
 }
 
